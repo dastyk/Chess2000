28c5b1b37e0736c15fec728c7b5ff79137d2a937
Home - Peter <dastykill@gmail.com>
2015-05-09 10:29:23 +0200

    left over

---
+++ Chess2000/Chess2000/PickState.cpp
@@ -0,0 +1,32 @@
+#include "PickState.h"
+
+
+PickState::PickState()
+{
+}
+
+
+PickState::~PickState()
+{
+}
+
+bool PickState::Update(float dt)
+{
+	return true;
+}
+
+
+bool PickState::Render()
+{
+	return true;
+}
+
+
+bool PickState::HandleInput()
+{
+	return true;
+}
+
+void PickState::StartGame()
+{
+}
